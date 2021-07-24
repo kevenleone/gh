de349fac29cd696ab46b5664e1a5c636c7310dfc
Keven Leone <keven.leone@liferay.com>
2021-07-24 18:53:41 -0300

    Add Eslint to the Project

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/standard",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "prettier",
+    "simple-import-sort",
+    "sort-destructure-keys",
+    "sort-keys-fix",
+  ],
+  rules: {
+    "@typescript-eslint/no-use-before-define": ["error"],
+    camelcase: "off",
+    "no-explicit-any": "off",
+    "no-use-before-define": "off",
+    semi: ["error", "always"],
+    "simple-import-sort/sort": "error",
+    "sort-destructure-keys/sort-destructure-keys": [
+      2,
+      { caseSensitive: false },
+    ],
+    "sort-keys": [
+      "error",
+      "asc",
+      { caseSensitive: true, minKeys: 2, natural: false },
+    ],
+    "sort-keys-fix/sort-keys-fix": "warn",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
