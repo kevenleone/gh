d0483689011829ffcbe00d7dbc925bcb2b404755
Keven Leone <keven.leone@liferay.com>
2021-11-24 23:27:29 -0300

    Ask for Jira Credential (Optional Question)

--- src/lib/credentials.ts
+++ src/lib/credentials.ts
@@ -1,8 +1,5 @@
-/// <reference types="zx"/>
-
-import prompts, { PromptObject } from "prompts";
-
 import { Configuration } from "../interfaces/types";
+import { prompts } from "./utils";
 
 const configFile = os.homedir() + "/.gh.github.json";
 
@@ -13,7 +10,9 @@ async function saveConfiguration(configuration: Configuration | any) {
 async function askConfiguration(
   initial?: Configuration
 ): Promise<Configuration> {
-  const questions: PromptObject[] = [
+  const jira: any = {};
+
+  const _credentials = await prompts([
     {
       initial: initial?.username,
       message: "What is your GitHub username?",
@@ -38,14 +37,52 @@ async function askConfiguration(
       name: "branch_prefix",
       type: "text",
     },
-  ];
-
-  const credentials = await prompts(questions, {
-    onCancel: () => {
-      console.log("No data will be saved");
-      process.exit(1);
+    {
+      initial: initial?.jira_integration,
+      message: "Do you want to use Jira Integration ?",
+      name: "jira_integration",
+      type: "confirm",
     },
-  });
+  ]);
+
+  if (_credentials.jira_integration) {
+    const [initial_jira_user, initial_jira_password] = Buffer.from(
+      initial?.jira_credential || "",
+      "base64"
+    )
+      .toString("ascii")
+      .split(":");
+
+    console.log(
+      chalk.cyan(
+        "👮 For your safety, password will be hidden and encrypted. Also it's only stored in your machine"
+      )
+    );
+
+    const { jira_password, jira_user } = await prompts([
+      {
+        initial: initial_jira_user,
+        message: "Jira User",
+        name: "jira_user",
+        type: "text",
+        validate: (user) => !!user,
+      },
+      {
+        hint: "For your safety, this password is hidden and encrypted. Also it's only stored in your machine",
+        initial: initial_jira_password,
+        message: "Jira Password",
+        name: "jira_password",
+        type: "password",
+        validate: (password) => !!password,
+      },
+    ]);
+
+    jira.jira_credential = Buffer.from(
+      `${jira_user}:${jira_password}`
+    ).toString("base64");
+  }
+
+  const credentials = { ...initial, ...jira, ..._credentials };
 
   await saveConfiguration(credentials as Configuration);
 
